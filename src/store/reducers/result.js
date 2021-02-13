9c0f658bddf593f01552bf3d79a0d76bb59257ba
giacomo9999 <gmarchesi888@gmail.com>
2021-02-13 16:36:57 -0500

    Complete through section 16.

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,22 +1,22 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = { results: [1, 2, 3] };
 
 const reducer = (state = initialState, action) => {
-  //   console.log("Reducer here", action.value);
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat(action.value),
-      };
+      });
+
     case actionTypes.DELETE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results
           .slice(0, action.index)
           .concat(state.results.slice(action.index + 1)),
-      };
+      });
+
     default:
       return state;
   }
